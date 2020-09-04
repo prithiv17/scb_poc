7bf434e3c9d4a43e831f45836cfe9c46b4a98dab
SwamyDammala <swamy.dammala13@gmail.com>
2020-09-04 09:37:02 +0530

    Changes in Registration form and added countires data

---
+++ src/Common/CountiresData.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+export const CountiresData= [
+    { country: 'Australia',states:[{name:'Queensland',city:['Brisbane','Townsville','Gladstone']},{name:'South Australia',city:['Adelaide','Gawler']},{name:'Victoria',city:['Melbourne','Bendigo']}]},
+    { country: 'India', states:[{name:'Andhra Pradesh',city:['Amaravathi','Vizag','Kakinada']},{name:'Tamilnadu',city:['Chennai','Madurai','Coimbatore']},{name:'Karnataka',city:['Bangalore','Mysore','Coorg']}]},
+    { country: 'USA', states:[{name:'Georgia',city:['Atlanta','Columbus','Roswell']},{name:'New york',city:['Rochester','Yonkers']},{name:'California',city:['Los Angeles','San Jose',]}]}
+]
+
+export const IdTypes=['Voter Id','Aadhar','Passport']
+
